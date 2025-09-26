a1841186760a0db82fac4bce315083cdf923b598
arshi_p17c5 <arshiyamoallem@cmail.carleton.ca>
2025-09-26 13:27:38 -0400

    changed line 21

---
+++ src/AddressBook.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddies;
+
+    public AddressBook() {
+        buddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddies.add(buddy);
+        }
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddies.remove(buddy);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+
+        BuddyInfo buddy = new BuddyInfo("John", "123 Cool Street");
+        AddressBook addressBook = new AddressBook();
+
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+}
